09e15adfa4db31acd2ae5dcc7afc87de0c85b58c
Otis Ma <littlehorseboy@gmail.com>
2019-12-12 14:21:09 +0800

    feat: 刪掉 muted，增加 preload: 'none'

--- ads.js
+++ ads.js
@@ -1,5 +1,4 @@
 var autoplayAllowed = false;
-var autoplayRequiresMute = false;
 var player;
 var wrapperDiv;
 
@@ -13,7 +12,6 @@ function checkUnmutedAutoplaySupport() {
       } else {
         // Unmuted autoplay is allowed.
         autoplayAllowed = true;
-        autoplayRequiresMute = false;
         initPlayer();
       }
     });
@@ -26,32 +24,34 @@ function checkMutedAutoplaySupport() {
       if (response.result === false) {
         // Muted autoplay is not allowed.
         autoplayAllowed = false;
-        autoplayRequiresMute = false;
       } else {
         // Muted autoplay is allowed.
         autoplayAllowed = true;
-        autoplayRequiresMute = true;
       }
       initPlayer();
     });
 }
 
 function initPlayer() {
-  var vjsOptions = {
+  player = videojs('content_video', {
+    controls: true,
     autoplay: autoplayAllowed,
-    muted: autoplayRequiresMute,
-    debug: true,
-  }
-  player = videojs('content_video', vjsOptions);
+    preload: 'none',
+    sources: [
+      {
+        src: '//commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        type: 'video/mp4',
+      },
+    ],
+  });
 
   var imaOptions = {
     id: 'content_video',
     adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&' +
       'iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&' +
       'impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&' +
-      'cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js' +
-      '&cmsid=496&vid=short_onecue&correlator='
-
+      'cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js&cmsid=496&' +
+      'vid=short_onecue&correlator=',
   };
   player.ima(imaOptions);
 
